9a5daf7230ac8b008b7be8c8b50fa7b87a1eedfb
AlbertSalia <15629610716@163.com>
2021-09-23 23:20:33 +0800

    p2PTest

--- run_suite.py
+++ run_suite.py
@@ -8,7 +8,8 @@ from script.login import login
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(login))
 
-report_file = app.BASE_DIR + "/report/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+# report_file = app.BASE_DIR + "/report/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+report_file = app.BASE_DIR + "/report/report.html"
 with open(report_file, "wb") as f:
     runner = HTMLTestRunner(f, title="P2P金融项目接口测试报告",description="test")
     runner.run(suite)
